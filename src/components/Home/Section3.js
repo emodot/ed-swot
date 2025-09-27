a1bdb816c738824c2d3cf0b4256427f0f8c52609
emodot <emayeodavid@gmail.com>
2025-09-27 17:44:41 +0100

    feat: developed section 3

---
+++ src/components/Home/Section3.js
@@ -0,0 +1,163 @@
+// import Section3AImg from "assets/images/home-sec-2a.jpg";
+import { ReactComponent as CourseIcon } from "assets/icons/course-icon.svg";
+import { ReactComponent as TutorIcon } from "assets/icons/tutor-icon.svg";
+import { ReactComponent as CertifiedIcon } from "assets/icons/certified-icon.svg";
+import { ReactComponent as ClockIcon } from "assets/icons/clock-icon.svg";
+import { ReactComponent as LocationIcon } from "assets/icons/location-icon.svg";
+// import Button from "components/Inputs/Button";
+import { useNavigate } from "react-router-dom";
+import Subject8 from "assets/images/subjects/subject-8.png";
+import TutorAvatar from "assets/images/mocks/tutor-avatar.png";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "variants.js";
+import Tabs from "components/Inputs/Tabs";
+import { useState } from "react";
+import { subjects } from "mocks/subjects";
+
+const Section3 = () => {
+  const allTabs = [
+    "Subjects",
+    "Language Courses",
+    "Life Skills",
+    "Software & Tech",
+    "Exam Prep",
+  ];
+  const navigate = useNavigate();
+  const [activeTab, setActiveTab] = useState(allTabs[0]);
+  // const isMobile = window.innerWidth < 768;
+
+  return (
+    <div className="bg-white">
+      <div className="max-w-[1300px] lg:w-[95%] w-[90%] m-auto pt-[5rem] pb-[6rem]">
+        <div className="flex flex-col items-center">
+          <motion.h1
+            className="font-albra_sans_sb text-[28px] lg:text-[32px] lg:leading-[40px] leading-[30px] text-center mb-4"
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+          >
+            Exceptional Learning, <br /> Redefined for You
+          </motion.h1>
+          <motion.p
+            className="font-aileron_r lg:text-18 text-14 text-center leading-[24px] mb-[3rem]"
+            variants={fadeIn("up", 0.4)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+          >
+            Learn smarter with personalized courses, real-time feedback, and a
+            support system built around you.
+          </motion.p>
+          <Tabs tabs={allTabs} onChange={setActiveTab} />
+        </div>
+        <div>
+          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
+            {subjects.map((course, idx) => (
+              <div
+                key={idx}
+                className="rounded-[12px] border border-neutral_stroke_2 overflow-hidden shadow-sm bg-white p-2"
+              >
+                <div className="relative">
+                  <img
+                    src={course.img}
+                    alt={course.title}
+                    className="w-full h-40 object-cover rounded-[12px] mb-4"
+                  />
+                  <div className="absolute top-2 left-2 flex gap-2 items-center bg-[#FFF2EB] rounded-[16px] px-4 py-1 w-fit">
+                    <CourseIcon />
+                    <p className="font-albra_sans_m text-dark_brand_primary text-14">
+                      Course
+                    </p>
+                  </div>
+                </div>
+                <div className="">
+                  <h3 className="text-16 font-aileron_sb text-brand_secondary">
+                    {course.title}{" "}
+                    {course.subtitle && (
+                      <span className="text-12 font-aileron_sb text-brand_secondary">
+                        ({course.subtitle})
+                      </span>
+                    )}
+                  </h3>
+                  <p className="text-14 font-aileron_r text-[#434343] mt-2">
+                    {course.desc}
+                  </p>
+
+                  <div className="flex items-center gap-1 mt-3 text-border_stroke_2">
+                    <span className="flex items-center gap-1 text-14">
+                      <CertifiedIcon /> Certified
+                    </span>
+                    <span className="h-4 border-l border-neutral_stroke_2 mx-2"></span>
+                    <span className="flex items-center gap-1 text-14 text-nowrap">
+                      <ClockIcon />
+                      90 Days
+                    </span>
+                    <span className="h-4 border-l border-neutral_stroke_2 mx-2"></span>
+                    <span className="flex items-center gap-1 text-14">
+                      <LocationIcon />
+                      Anywhere
+                    </span>
+                  </div>
+
+                  <div className="flex items-center gap-2 mt-4">
+                    <img
+                      src={TutorAvatar}
+                      alt="tutor avatar"
+                      className="h-10"
+                    />
+                    <div>
+                      <p className="text-14 font-aileron_sb text-brand_secondary">
+                        {course.tutor}
+                      </p>
+                      <p className="text-12 text-border_stroke_2">
+                        Science Professor
+                      </p>
+                    </div>
+                    <div className="ml-auto flex gap-1 items-center bg-[#EBF5FF] rounded-[16px] px-3 py-1 w-fit">
+                      <TutorIcon />
+                      <p className="font-albra_sans_m text-[#166EB4] text-14">
+                        Tutor
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="flex gap-3 mt-6">
+                    <button className="flex-1 bg-brand_primary text-brand_secondary font-aileron_r text-14 py-2 rounded-md">
+                      Get Started
+                    </button>
+                    <button className="flex-1 text-brand_secondary font-aileron_r text-14 py-2 rounded-md">
+                      View Details
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+
+            {/* Special Card */}
+            <div className="border border-neutral_stroke_2 rounded-[12px] text-white p-2">
+              <img
+                src={Subject8}
+                alt={"contact us"}
+                className="w-full h-40 object-cover rounded-[12px] mb-4"
+              />
+              <h3 className="text-16 font-aileron_sb text-brand_secondary">
+                Can’t Find The Course You Need?
+              </h3>
+              <p className="text-14 font-aileron_r text-[#434343] mt-2 mb-4">
+                Let us know what you’re looking for, we might already be working
+                on it!
+              </p>
+              <button className="bg-brand_primary text-brand_secondary font-aileron_r text-14 px-6 py-2 rounded-md">
+                Contact Us
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section3;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import Section1 from "components/Home/Section1";
-// import Section2 from "components/Home/Section2";
-// import Section3 from "components/Home/Section3";
+import Section2 from "components/Home/Section2";
+import Section3 from "components/Home/Section3";
 // import Section4 from "components/Home/Section4";
 import SEO from "../components/SEO";
 import { useEffect } from "react";
@@ -18,8 +18,8 @@ const Home = () => {
         type="website"
       />
       <Section1 />
-      {/* <Section2 /> */}
-      {/* <Section3 /> */}
+      <Section2 />
+      <Section3 />
       {/* <Section4 /> */}
     </>
   );
